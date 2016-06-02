25a8627f3e1ac73b44f7a50055c860a2d42cf79a
Erwin De Ley <erwindl0@gmail.com>
2016-06-02 12:57:05 +0200

    First real commit incl sample console test cmd
    Signed-off-by: Erwin De Ley <erwindl0@gmail.com>

---
+++ uk.ac.diamond.scisoft.analysis.rpc/src/main/java/uk/ac/diamond/scisoft/analysis/rpc/flattening/helpers/PassThroughFlatteningHelper.java
@@ -0,0 +1,41 @@
+/*******************************************************************************
+ * Copyright (c) 2014-2016 Diamond Light Source Ltd., 
+ *                         Kichwa Coders & iSencia Belgium NV.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    DLS, Kichwa Coders - initial API and implementation and/or initial documentation
+ *    Erwin De Ley - extraction from DAWN to ease reuse in other contexts
+ *******************************************************************************/
+
+package uk.ac.diamond.scisoft.analysis.rpc.flattening.helpers;
+
+import uk.ac.diamond.scisoft.analysis.rpc.flattening.IFlattener;
+import uk.ac.diamond.scisoft.analysis.rpc.flattening.IRootFlattener;
+
+public class PassThroughFlatteningHelper implements IFlattener<Object> {
+
+	@Override
+	public Object flatten(Object obj, IRootFlattener rootFlattener) {
+		return obj;
+	}
+
+	@Override
+	public Object unflatten(Object obj, IRootFlattener rootFlattener) {
+		return obj;
+	}
+
+	@Override
+	public boolean canFlatten(Object obj) {
+		return obj instanceof Integer || obj instanceof Boolean || obj instanceof String || obj instanceof Double
+				|| obj instanceof byte[];
+	}
+
+	@Override
+	public boolean canUnFlatten(Object obj) {
+		return canFlatten(obj);
+	}
+}
